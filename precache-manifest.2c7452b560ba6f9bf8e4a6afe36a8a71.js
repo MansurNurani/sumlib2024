67da055a679b381c62f24fe70f5fa6eeae5c50e1
MansurNurani <mansur.nurani@gmail.com>
2021-09-11 16:16:27 +0430

    Updates

---
+++ precache-manifest.2c7452b560ba6f9bf8e4a6afe36a8a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3bac0de9980ffe79b3cde7c4949a2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72d1f1c087cf1386c0d1",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "009dd0078b0da63176ec",
+    "url": "./static/js/2.30f21851.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.30f21851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d1f1c087cf1386c0d1",
+    "url": "./static/js/main.7c840881.chunk.js"
+  },
+  {
+    "revision": "0954842b1040ea14bc91",
+    "url": "./static/js/runtime-main.654ae7a6.js"
+  }
+]);
